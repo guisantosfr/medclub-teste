ff43f1a5fd8a2a656461ffbc496bb0895d2e2cc4
Guilherme Santos <santosgui678@gmail.com>
2025-08-11 20:48:04 -0300

    feat: fab to new consultation

--- src/screens/ConsultationsListScreen.tsx
+++ src/screens/ConsultationsListScreen.tsx
@@ -6,11 +6,22 @@ import type { RootStackParamList } from '../types/RootStackParamList'
 import { consultations } from '../utils/mockConsultations'
 import ConsultationCard from '../components/ConsultationCard';
 import { SafeAreaView } from "react-native-safe-area-context"
+import { AnimatedFAB } from "react-native-paper"
+import { useState } from "react"
 
 type Nav = NativeStackNavigationProp<RootStackParamList, 'ConsultationsList'>
 
 export default function ConsultationsListScreen() {
     const navigation = useNavigation<Nav>()
+
+    const [isExtended, setIsExtended] = useState(true);
+
+    const onScroll = ({ nativeEvent }) => {
+    const currentScrollPosition =
+      Math.floor(nativeEvent?.contentOffset?.y) ?? 0;
+
+    setIsExtended(currentScrollPosition <= 0);
+  };
 
     return (
         <SafeAreaView style={styles.container}>
@@ -18,22 +29,31 @@ export default function ConsultationsListScreen() {
                 <View style={styles.emptyWrap}>
                     <Text style={styles.emptyTitle}>Sem consultas</Text>
                     <Text style={styles.emptyText}>Toque em "Nova" para agendar sua primeira consulta.</Text>
-                    <Button title="Nova consulta" onPress={() => {navigation.navigate('AddConsultation')}}></Button>
+                    <Button title="Nova consulta" onPress={() => { navigation.navigate('AddConsultation') }}></Button>
                 </View>
             ) : (
                 <>
-                <FlatList
-                    data={consultations}
-                    keyExtractor={item => item.id.toString()}
-                    ItemSeparatorComponent={() => <View style={{ height: 15 }} />}
-                    contentContainerStyle={{ padding: 16, paddingBottom: 24 }}
-                    style={styles.cardList}
-                    renderItem={({ item }) => (
-                        <ConsultationCard item={item} onPress={() => navigation.navigate('ConsultationDetails', { id: item.id.toString() })} />
-                    )}
-                />
-                                    <Button title="Nova consulta" onPress={() => {navigation.navigate('AddConsultation')}}></Button>
-
+                    <FlatList
+                        data={consultations}
+                        keyExtractor={item => item.id.toString()}
+                        ItemSeparatorComponent={() => <View style={{ height: 15 }} />}
+                        contentContainerStyle={{ padding: 16, paddingBottom: 24 }}
+                        style={styles.cardList}
+                        renderItem={({ item }) => (
+                            <ConsultationCard item={item} onPress={() => navigation.navigate('ConsultationDetails', { id: item.id.toString() })} />
+                        )}
+                        onScroll={onScroll}
+                    />
+                    {/* <Button title="Nova consulta" onPress={() => {navigation.navigate('AddConsultation')}}></Button> */}
+                    <AnimatedFAB
+                        icon={'plus'}
+                        label={'Nova consulta'}
+                        extended={isExtended}
+                        onPress={() => {navigation.navigate('AddConsultation')}}
+                        animateFrom={'right'}
+                        iconMode={'static'}
+                        style={styles.fabStyle}
+                    />
                 </>
             )}
         </SafeAreaView>
@@ -53,6 +73,12 @@ const styles = StyleSheet.create({
     emptyTitle: { fontSize: 18, fontWeight: '700', color: '#111827' },
     emptyText: { fontSize: 14, color: '#6b7280', textAlign: 'center' },
     cardList: {
-        maxHeight: '90%'
-    }
+        maxHeight: '92.5%'
+    },
+    fabStyle: {
+        bottom: 60,
+        right: 20,
+        position: 'absolute',
+    },
+
 })

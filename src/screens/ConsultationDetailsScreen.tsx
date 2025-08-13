f67999ee9c46ba5fd24a43694ab96392bb3a0c07
Guilherme Santos <santosgui678@gmail.com>
2025-08-13 20:11:13 -0300

    style: consultation details screen

--- src/screens/ConsultationDetailsScreen.tsx
+++ src/screens/ConsultationDetailsScreen.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { View, Text, StyleSheet, Alert, Pressable } from 'react-native'
+import { View, StyleSheet, Alert, Pressable } from 'react-native'
 import type { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 import { RootStackParamList } from '../types/RootStackParamList'
 import { useConsultations } from "../contexts/ConsultationsContext";
-import { Appbar } from 'react-native-paper';
+import { Appbar, Avatar, Button, Card, Text } from 'react-native-paper';
 import Toast from 'react-native-toast-message';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'ConsultationDetails'>
@@ -14,6 +14,26 @@ export default function ConsultationDetailsScreen({ route, navigation }: Props) 
   const { getById, remove } = useConsultations();
 
   const consultation = getById(id);
+
+  // COMPONENTIZAR ESSA FUNÇÃO
+  function formatDate(dateStr: string) {
+    const [y, m, d] = dateStr.split("-").map(Number);
+    const date = new Date(y, m - 1, d);
+
+    const day = String(date.getDate()).padStart(2, "0");
+    const monthIndex = date.getMonth();
+    const monthNum = String(monthIndex + 1).padStart(2, "0");
+    const year = date.getFullYear();
+    const currentYear = new Date().getFullYear();
+
+    const showYear = year !== currentYear;
+
+    const version2 = showYear
+      ? `${day}/${monthNum}/${year}`
+      : `${day}/${monthNum}`;
+
+    return version2;
+  }
 
   if (!consultation) {
     return (
@@ -46,56 +66,88 @@ export default function ConsultationDetailsScreen({ route, navigation }: Props) 
   return (
     <View style={styles.container}>
       <Appbar.Header>
+        <Appbar.BackAction onPress={() => { navigation.goBack() }} />
         <Appbar.Content title="Detalhes da consulta" />
       </Appbar.Header>
 
-      <View style={styles.card}>
-        <Row label="Data">{consultation.date}</Row>
-        <Row label="Hora">{consultation.time}</Row>
-        <Row label="Médico(a)">{consultation.doctor}</Row>
-        <Row label="Especialidade">{consultation.specialty}</Row>
-        <Row label="Localização">{consultation.location}</Row>
-      </View>
+      <Card style={styles.doctorCard}>
+        <Card.Title
+          title={consultation.doctor}
+          titleVariant='titleLarge'
+          subtitle={consultation.specialty}
+          subtitleVariant='titleMedium'
+          subtitleStyle={styles.subtitle}
+          left={(props) => <Avatar.Icon {...props} icon="account" size={50} />}
+          leftStyle={styles.avatar}>
+        </Card.Title>
+      </Card>
 
-      <Pressable
-        onPress={handleDelete}
-        style={({ pressed }) => [styles.deleteBtn, pressed && { opacity: 0.85 }]}
-        accessibilityRole="button"
-        accessibilityLabel="Excluir esta consulta"
+      <Card style={styles.consultationInfoCard}>
+        <Card.Content>
+          <View style={styles.consultationInfo}>
+            <Text variant="bodyLarge">Data</Text>
+            <Text variant="titleMedium">{formatDate(consultation.date)}</Text>
+          </View>
+
+          <View style={styles.consultationInfo}>
+            <Text variant="bodyLarge">Horário</Text>
+            <Text variant="titleMedium">{consultation.time}</Text>
+          </View>
+
+          <View style={styles.consultationInfo}>
+            <Text variant="bodyLarge">Localização</Text>
+            <Text variant="titleMedium">{consultation.location}</Text>
+          </View>
+
+        </Card.Content>
+      </Card>
+
+      <Button 
+      mode='contained' 
+      buttonColor='#ef4444'
+      onPress={handleDelete}
+      style={styles.deleteButton}
+      icon="trash-can"
       >
-        <Text style={styles.deleteText}>Excluir consulta</Text>
-      </Pressable>
-    </View>
-  )
-}
-
-function Row({ label, children }: { label: string; children: React.ReactNode }) {
-  return (
-    <View style={styles.row}>
-      <Text style={styles.label}>{label}</Text>
-      <Text style={styles.value}>{children}</Text>
+        Excluir consulta
+      </Button>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#f9fafb', padding: 16, gap: 16 },
-  card: {
-    backgroundColor: '#ffffff',
-    borderRadius: 12,
-    borderWidth: 1,
-    borderColor: '#e5e7eb',
-    padding: 14,
-    gap: 12,
+  container: { 
+    flex: 1,
+    padding: 16,
+    gap: 16
   },
-  row: { gap: 2 },
-  label: { fontSize: 12, color: '#6b7280' },
-  value: { fontSize: 16, color: '#111827', fontWeight: '600' },
-  deleteBtn: {
-    backgroundColor: '#ef4444',
+  
+  doctorCard: {
     paddingVertical: 12,
-    borderRadius: 10,
-    alignItems: 'center',
+    marginVertical: 6
   },
-  deleteText: { color: '#ffffff', fontWeight: '700' },
+
+  consultationInfoCard: {
+    marginVertical: 6
+  },
+
+  subtitle: {
+    color: 'gray'
+  },
+
+  avatar: {
+    marginRight: 32,
+  },
+
+  consultationInfo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 8
+  },
+
+  deleteButton: {
+    width: '75%',
+    marginHorizontal: 'auto',
+    marginTop: 8
+  },
 })

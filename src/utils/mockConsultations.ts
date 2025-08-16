d86b30981c669194807e9bfa3d3f4abacc06aaad
Guilherme Santos <santosgui678@gmail.com>
2025-08-16 19:25:24 -0300

    refactor: change mock data

--- src/utils/mockConsultations.ts
+++ src/utils/mockConsultations.ts
@@ -1,6 +1,6 @@
 export const mockConsultations = [
     {
-        date: "2025-08-15",
+        date: "2025-08-16",
         doctor: "Dr. João Silva",
         id: "1",
         location: "Hospital São Marcos",
@@ -9,7 +9,7 @@ export const mockConsultations = [
         canceled: false
     },
     {
-        date: "2025-08-16",
+        date: "2025-08-17",
         doctor: "Dra. Ana Beatriz Costa",
         id: "2",
         location: "Hospital Getúlio Vargas",
@@ -18,10 +18,19 @@ export const mockConsultations = [
         canceled: false
     },
     {
-        date: "2025-08-17",
+        date: "2025-08-18",
         doctor: "Dra. Fernanda Oliveira",
         id: "3",
-        location: "Med Image",
+        location: "Med Imagem",
+        specialty: "Ortopedista",
+        time: "09:00",
+        canceled: false
+    },
+    {
+        date: "2025-08-19",
+        doctor: "Dr. Rafael Martins",
+        id: "3",
+        location: "Med Imagem",
         specialty: "Ortopedista",
         time: "09:00",
         canceled: true
